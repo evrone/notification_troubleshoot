382b2f1aa12f3e6f91188d396c48afe256c77daa
britov <britovmisha@gmail.com>
2021-03-23 17:46:47 +0300

    code cleanup

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'dart:async';
 
-import 'package:flutter/services.dart';
 import 'package:notification_troubleshoot/notification_troubleshoot.dart';
 
 void main() {
--- test/notification_troubleshoot_test.dart
+++ test/notification_troubleshoot_test.dart
@@ -1,5 +1,1 @@
-import 'package:flutter/services.dart';
-import 'package:flutter_test/flutter_test.dart';
-import 'package:notification_troubleshoot/notification_troubleshoot.dart';
-
 void main() {}
